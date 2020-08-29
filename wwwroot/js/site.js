306502cddf01200ea43f185c76d1408020605343
StephenBrooks220413 <46081327+StephenBrooks220413@users.noreply.github.com>
2020-08-29 17:49:39 -0500

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Write your JavaScript code//
+/*breakfast menu*/
+function breakfast() {
+    document.getElementById("breakfast").innerHTML = "Eggs, bacon, potatoes, toast for $12.99";
+}
+
+
+/*brunch menu*/
+function brunch() {
+    document.getElementById("brunch").innerHTML = "Sandwhiches, soups, salads for $10.49";
+}
+
+
+/*lunch menu*/
+function lunch() {
+    document.getElementById("lunch").innerHTML = "Sandwhiches, chips, chicken for $11.50";
+}
+
+
+/*dinner menu*/
+function dinner() {
+    document.getElementById("dinner").innerHTML = "Steaks, ribs, sausages for $14.37";
+}
