2595a7c25b61c790071b0a59f438b6d075519285
StephenBrooks220413 <46081327+StephenBrooks220413@users.noreply.github.com>
2020-08-31 07:43:15 -0500

    updates to Sign in features

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -21,3 +21,14 @@ function lunch() {
 function dinner() {
     document.getElementById("dinner").innerHTML = "Steaks, ribs, sausages for $14.37";
 }
+
+
+
+/*Google JS*/
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
